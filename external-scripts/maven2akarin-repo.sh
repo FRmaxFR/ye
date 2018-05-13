398cc5a80e95a230c50a4a8df41269420cc3037d
Sotr <i@omc.hk>
2018-05-13 16:25:00 +0800

    [Deploy] Update LegacyLauncher 1.19

--- external-scripts/maven2akarin-repo.sh
+++ external-scripts/maven2akarin-repo.sh
@@ -1,6 +1,7 @@
 #! /bin/bash 
 
 basedir='' 
+workdir=$(cd $(dirname $0); pwd)
 proj_id=$(mvn -q -N -Dexec.executable="echo"  -Dexec.args='${project.artifactId}'  org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 proj_version=$(mvn -q -N -Dexec.executable="echo"  -Dexec.args='${project.version}'  org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 repo_path=$basedir'akarin-repo/repository/'
@@ -11,6 +12,7 @@ if [ ! -d $basedir'akarin-repo' ]; then
  cd $basedir
  git clone 'https://github.com/Akarin-project/Akarin-repo.git'
  echo '[Deploy] Synchronized remote!'
+ cd $workdir
 fi
 
 echo '[Deploy] Deploy to local repository..'
